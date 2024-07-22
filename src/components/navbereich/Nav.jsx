d7498c42e8e78ccdb6121c43d04ae095fbb340bb
Michael Dent <dentmichael@aol.com>
2024-07-22 11:47:11 +0200

    spa-dog-motel

--- src/components/navbereich/Nav.jsx
+++ src/components/navbereich/Nav.jsx
@@ -2,37 +2,20 @@ import "./style.css";
 const Nav = () => {
   return (
     <>
-      <div className="nav-image">
-        <img
-          src="./images/megi.jpg"
-          className="img-fluid"
-          width="300px"
-          height="300px"
-        />
-        <img
-          src="./images/dog2.jpg"
-          className="img-fluid"
-          width="300px"
-          height="300px"
-        />
-        <img
-          src="./images/dog3.jpg"
-          className="img-fluid"
-          width="300px"
-          height="300px"
-        />
-        <img
-          src="./images/dachshund.jpg"
-          className="img-fluid"
-          width="300px"
-          height="300px"
-        />
-      </div>
-
-      <h2>
-        Herzlich Willkommen im Hunde-Motel &quot;Pfotenparadies&quot;! 🐾🐶❤️
-      </h2>
-      <h3>Bei uns dreht sich alles um das Wohl Ihrer pelzigen Freund!</h3>
+      <header className="header-container">
+        <div className="header-image-container">
+          <img src="./images/megi.jpg" className="header-img" />
+        </div>
+        <div className="header-text-container">
+          <h1 className="header-title">
+            Herzlich Willkommen im Hunde-Motel &quot;Pfotenparadies&quot;!
+            🐾🐶❤️
+          </h1>
+          <p className="header-subtitle">
+            Bei uns dreht sich alles um das Wohl Ihrer pelzigen Freund!
+          </p>
+        </div>
+      </header>
 
       <nav>
         <div className="topnav">
--- src/pages/Angebote/Angebote.jsx
+++ src/pages/Angebote/Angebote.jsx
@@ -1,16 +1,26 @@
-import React from "react";
-
 const Angebote = () => {
   return (
     <div>
       <h1>Angebote</h1>
-      <p>
-        Tierärzte, T-Heiprak.,T-Physiotherapeuten, Hundetrainer, WebDevs,
-        Werbepartner, Booking-Portale, Tierheime, Hundefriseure (auch mobil),
-        Toys & T-Bedarf, Hundetaxi, Reiseanbieter mit Hundepaket, Versicherungen
-        (Check24), Lokale Parks, Naturreservate, Hundecafe´, Restaurant
-        f.Dog/Owner, Playstore, Facebook, Instagramm, TikTok,{"hkjhkhjlkhlk "}
-      </p>
+      <br></br>
+      <h2>Unsere Partner sind</h2>
+      <ul>
+        <li>Tierärzte</li>
+        <li>Tierheilpraktiker</li>
+        <li>Physiotherapeuteh</li>
+        <li>Hundetrainer</li>
+        <li>Webentwickler</li>
+        <li>Diverse Werbepartner</li>
+        <li>Tierheime</li>
+        <li>Hundefriseure a.mobil</li>
+        <li>Tierbedarf z.B. Toys</li>
+        <li></li>
+        T-Heiprak.,T-Physiotherapeuten, Hundetrainer, WebDevs, Werbepartner,
+        Booking-Portale, Tierheime, Hundefriseure (auch mobil), Toys & T-Bedarf,
+        Hundetaxi, Reiseanbieter mit Hundepaket, Versicherungen (Check24),
+        Lokale Parks, Naturreservate, Hundecafe´, Restaurant f.Dog/Owner,
+        Playstore, Facebook, Instagramm, TikTok,
+      </ul>
     </div>
   );
 };
